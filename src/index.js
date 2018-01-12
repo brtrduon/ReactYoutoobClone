1ea8f11081f710f1ca39fe8be02c59a912de8076
unknown <you@example.com>
2018-01-12 08:47:21 -0800

    working on adding ajax requests

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyAhPlUCJLxGNidT2Ja1hLvKi0wi3KmgFEM';
 
+
 // create a new component. this component should produce some HTML
-const App = () => {
-    return <div>
-        <SearchBar />
-    </div>;
+class App extends Component {
+    constructor(props) {
+        // constructor always gets called with props
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+            // in ES6, when the key and var are the same name, we can simply type it as the line above
+            // i.e. the line above is equivalent to this.setState({ videos: videos })
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 // take this component's generated HTML and put it on the page (in the DOM)
